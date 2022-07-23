3f30e09c512ca1ac04f0b648dc1832a975d26133
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-22 22:14:47 -0700

    3 down, these are weird

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,31 @@
 module Enumerable
-  # Your code goes here
+  # stuck with self.length because I think it's faster
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    return self
+  end
+
+  def my_select
+    list = []
+    self.each do |y|
+      list.push(y) if yield(y)
+    end 
+    list
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+    return self
+  end
 end
